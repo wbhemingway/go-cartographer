1096c8bfcfa191dde6b0c96a72538ebcf5b5b01f
wbhemingway <william.b.hemingway@gmail.com>
2026-03-05 10:11:29 -0500

    added more assets, reads json

--- cmd/sb/main.go
+++ cmd/sb/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 
 	"github.com/fogleman/gg"
 	"github.com/wbhemingway/go-cartographer/internal/renderer"
@@ -10,16 +12,14 @@ import (
 func main() {
 	cfg := renderer.DefaultConfig()
 	engine := renderer.New(cfg)
-
-	world := renderer.World{
-		Width:  2,
-		Height: 2,
-		Tiles: []renderer.Tile{
-			{X: 0, Y: 0, Terrain: "grass"},
-			{X: 1, Y: 0, Terrain: "grass", Structure: "tree", Creature: "goblin"},
-			{X: 0, Y: 1, Terrain: "dirt", Creature: "goblin"},
-			{X: 1, Y: 1, Terrain: "water"},
-		},
+	world_file, err := os.ReadFile("sandbox_map.json")
+	if err != nil {
+		log.Fatalf("Failed to read map file: %v", err)
+	}
+	var world renderer.World
+	err = json.Unmarshal(world_file, &world)
+	if err != nil {
+		log.Fatalf("Failed to parse JSON: %v", err)
 	}
 
 	log.Println("Rendering map...")
